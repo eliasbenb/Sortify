2679d72c64f906eeba4db9872e6a74530b0af669
Elias <eliasbenbouenane@gmail.com>
2020-04-27 22:43:09 +0400

    Made login info stored in cookies and added support to sort more than 100 tracks

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template, redirect, request, session, make_response,session,redirect
+from flask import Flask, render_template, redirect, request, session, make_response, url_for
 from datetime import datetime
-import logging, os, requests, spotipy, sys, time
+import os, requests, spotipy, sys, time
 import spotipy.util as util
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
 
-def get_tracks(spotipy_obj, username, playlist_ids):
-    results = spotipy_obj.user_playlist(username, playlist_ids, fields="tracks, next")
+def get_tracks(spotipy_obj, username, playlist_id):
+    results = spotipy_obj.user_playlist(username, playlist_id, fields="tracks, next")
     temp_tracks = results["tracks"]
 
     original_tracks = temp_tracks["items"]
@@ -21,9 +19,23 @@ def get_tracks(spotipy_obj, username, playlist_ids):
 
     return original_tracks
 
+def replace_tracks(sorted_tracks, sp, playlist_id, username):
+    track_ids = []
+    for sorted_track in sorted_tracks:
+        track_ids.append(sorted_track['track']['id'])
+    
+    if len(track_ids) > 100:
+        chunks = [track_ids[x:x+100] for x in range(0, len(track_ids), 100)]
+        sp.user_playlist_replace_tracks(username, playlist_id, "")
+        for track_ids in chunks:
+            sp.user_playlist_add_tracks(username, playlist_id, track_ids)
+    else:
+        sp.user_playlist_replace_tracks(username, playlist_id, track_ids)
+    
+
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-SCOPE = 'playlist-modify-private,playlist-modify-public,user-top-read'
+SCOPE = 'playlist-read-collaborative,playlist-read-private,playlist-modify-public,playlist-modify-private'
 SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 API_BASE = 'https://accounts.spotify.com'
 SHOW_DIALOG = True
@@ -54,12 +66,7 @@ def callback():
 
     res_body = res.json()
     print(res.json())
-    session["toke"] = res_body.get("access_token")
-
-    global sp
-    global username
-    sp = spotipy.Spotify(auth=session['toke'])
-    username = sp.current_user()['id']
+    session["token"] = res_body.get("access_token")
     
     return redirect("login_success")
 
@@ -69,89 +76,115 @@ def login_success_page():
 
 @app.route('/playlists')
 def playlists_page():
-    time.sleep(1.5)
+    time.sleep(0.5)
     try:
-        print(sp)
-        global playlists_soup
+        sp = spotipy.Spotify(auth=session['token'])
         playlists_soup = sp.current_user_playlists()
         return render_template('playlists.html', playlists_soup = playlists_soup)
     except:
         return redirect("login")
 
+@app.route('/not_your_playlist')
+def not_your_playlist_page():
+    return render_template('not_your_playlist.html')
+
 @app.route('/playlists/<int:index>')
 def playlist_page(index):
+    sp = spotipy.Spotify(auth=session['token'])
+    username = sp.current_user()['id']
+    playlists_soup = sp.current_user_playlists()
     playlist = playlists_soup['items'][index-1]
-    playlist_name = playlist['name']
     playlist_id = playlist['id']
-    global original_tracks
+
     original_tracks = []
     original_tracks.extend(get_tracks(sp, username, playlist_id))
     results = sp.playlist(playlist['id'],
         fields="tracks,next")
     tracks = results['tracks']
-    for track in tracks['items']:
-        print(track['added_at'])
-    return render_template('playlist.html', playlist = playlist, playlist_name = playlist_name, playlist_id = playlist_id, playlists_soup = playlists_soup, tracks = tracks, index = index)
-
-@app.route('/playlists/<int:index>/added_at')
-def sort_by_added_at(index):
-    playlist_name = playlists_soup['items'][index-1]['name']
-    playlist_id = playlists_soup['items'][index-1]['id']
-    sorted_tracks = sorted(original_tracks,
-                    key=lambda k: datetime.strptime(k["added_at"], "%Y-%m-%dT%H:%M:%SZ"), reverse=True)
-    track_ids = []
-    for sorted_track in sorted_tracks:
-        track_ids.append(sorted_track['track']['id'])
-    sp.user_playlist_replace_tracks(username, playlist_id, track_ids)
-    return render_template('sorted.html', playlist_name = playlist_name, index = index)
+    return render_template('playlist.html', playlist = playlist, tracks = tracks, index = index)
 
 @app.route('/playlists/<int:index>/alphabetical-az')
 def sort_by_alphabetical_az(index):
-    playlist_name = playlists_soup['items'][index-1]['name']
-    playlist_id = playlists_soup['items'][index-1]['id']
+    sp = spotipy.Spotify(auth=session['token'])
+    username = sp.current_user()['id']
+    playlists_soup = sp.current_user_playlists()
+    playlist = playlists_soup['items'][index-1]
+    playlist_id = playlist['id']
+
+    if playlist['owner']['id'] != username:
+        return redirect (url_for("not_your_playlist_page"))
+
+    original_tracks = []
+    original_tracks.extend(get_tracks(sp, username, playlist_id))
+
     sorted_tracks = sorted(original_tracks,
                     key=lambda k: k['track']["name"], reverse=False)
-    track_ids = []
-    for sorted_track in sorted_tracks:
-        track_ids.append(sorted_track['track']['id'])
-    sp.user_playlist_replace_tracks(username, playlist_id, track_ids)
-    return render_template('sorted.html', playlist_name = playlist_name, index = index)
+    replace_tracks(sorted_tracks, sp, playlist_id, username)
+    return render_template('sorted.html', playlist = playlist, index = index)
 
 @app.route('/playlists/<int:index>/alphabetical-za')
 def sort_by_alphabetical_za(index):
-    playlist_name = playlists_soup['items'][index-1]['name']
-    playlist_id = playlists_soup['items'][index-1]['id']
+    sp = spotipy.Spotify(auth=session['token'])
+    username = sp.current_user()['id']
+    playlists_soup = sp.current_user_playlists()
+    playlist = playlists_soup['items'][index-1]
+    playlist_id = playlist['id']
+
+    original_tracks = []
+    original_tracks.extend(get_tracks(sp, username, playlist_id))
+
     sorted_tracks = sorted(original_tracks,
                     key=lambda k: k['track']["name"], reverse=True)
-    track_ids = []
-    for sorted_track in sorted_tracks:
-        track_ids.append(sorted_track['track']['id'])   
-    sp.user_playlist_replace_tracks(username, playlist_id, track_ids)
-    return render_template('sorted.html', playlist_name = playlist_name, index = index)
+    replace_tracks(sorted_tracks, sp, playlist_id, username)
+    return render_template('sorted.html', playlist = playlist, index = index)
 
 @app.route('/playlists/<int:index>/release_date-chronological')
 def sort_by_release_date_chronological(index):
-    playlist_name = playlists_soup['items'][index-1]['name']
-    playlist_id = playlists_soup['items'][index-1]['id']
+    sp = spotipy.Spotify(auth=session['token'])
+    username = sp.current_user()['id']
+    playlists_soup = sp.current_user_playlists()
+    playlist = playlists_soup['items'][index-1]
+    playlist_id = playlist['id']
+
+    original_tracks = []
+    original_tracks.extend(get_tracks(sp, username, playlist_id))
+
     sorted_tracks = sorted(original_tracks,
                     key=lambda k: k['track']['album']['release_date'], reverse=True)
-    track_ids = []
-    for sorted_track in sorted_tracks:
-        track_ids.append(sorted_track['track']['id'])    
-    sp.user_playlist_replace_tracks(username, playlist_id, track_ids)
-    return render_template('sorted.html', playlist_name = playlist_name, index = index)
+    replace_tracks(sorted_tracks, sp, playlist_id, username)
+    return render_template('sorted.html', playlist = playlist, index = index)
 
 @app.route('/playlists/<int:index>/release_date-non_chronological')
 def sort_by_release_date_non_chronological(index):
-    playlist_name = playlists_soup['items'][index-1]['name']
-    playlist_id = playlists_soup['items'][index-1]['id']
+    sp = spotipy.Spotify(auth=session['token'])
+    username = sp.current_user()['id']
+    playlists_soup = sp.current_user_playlists()
+    playlist = playlists_soup['items'][index-1]
+    playlist_id = playlist['id']
+
+    original_tracks = []
+    original_tracks.extend(get_tracks(sp, username, playlist_id))
+
     sorted_tracks = sorted(original_tracks,
                     key=lambda k: k['track']['album']['release_date'], reverse=False)
-    track_ids = []
-    for sorted_track in sorted_tracks:
-        track_ids.append(sorted_track['track']['id'])    
-    sp.user_playlist_replace_tracks(username, playlist_id, track_ids)
-    return render_template('sorted.html', playlist_name = playlist_name, index = index)
+    replace_tracks(sorted_tracks, sp, playlist_id, username)
+    return render_template('sorted.html', playlist = playlist, index = index)
+
+@app.route('/playlists/<int:index>/added_at')
+def sort_by_added_at(index):
+    sp = spotipy.Spotify(auth=session['token'])
+    username = sp.current_user()['id']
+    playlists_soup = sp.current_user_playlists()
+    playlist = playlists_soup['items'][index-1]
+    playlist_id = playlist['id']
+
+    original_tracks = []
+    original_tracks.extend(get_tracks(sp, username, playlist_id))
+
+    sorted_tracks = sorted(original_tracks,
+                    key=lambda k: datetime.strptime(k["added_at"], "%Y-%m-%dT%H:%M:%SZ"), reverse=True)
+    replace_tracks(sorted_tracks, sp, playlist_id, username)
+    return render_template('sorted.html', playlist = playlist, index = index)
 
 if __name__ == "__main__":
     app.run()
